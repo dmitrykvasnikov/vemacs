d1f16d1d957b1945e83a6e860cd8ab38680e443d
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2026-06-10 18:06:59 +0200

    minor fixes

--- custom.el
+++ custom.el
@@ -4,13 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons-dired consult-projectile corfu-terminal diredfl
-			 doom-modeline doom-themes ef-themes eldoc-box
-			 embark-consult expand-region flymake-popon
-			 format-all gruvbox-theme haskell-mode helpful
-			 kind-icon marginalia orderless
-			 rainbow-delimiters spacemacs-theme vertico)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
